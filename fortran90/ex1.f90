8d0ec4761d4f5be234a3d9912a700ee210ad3df5
mhrusz <mhrusz+github@gmail.com>
2016-07-08 10:26:29 +0200

    Fortran excercise fixed with reduction.

--- fortran90/ex1.f90
+++ fortran90/ex1.f90
@@ -1,23 +1,24 @@
 PROGRAM ex1
+  use omp_lib
+
   IMPLICIT NONE
 
-  integer, parameter :: num_threads = 4
-  integer, parameter :: num_steps = 100000
-  real(kind=8) :: step
-  real(kind=8) :: pi,x
-  real(kind=8) :: sum = 0.0
+  integer :: num_steps = 100000
+  real :: step
+  real :: pi, x
+  real :: summ
   integer :: i
 
   step = 1.0/num_steps
 
-  !$OMP PARALLEL DO
-  do i=0,1,num_steps
+  summ = 0.0
+  !$OMP PARALLEL DO REDUCTION(+:summ)
+  do i=0, num_steps
     x = (i+0.5)*step
-    sum = sum + 4.0/(1.0+ x*x)
+    summ = summ + 4.0/(1.0+ x*x)
   end do
   !$OMP END PARALLEL DO
 
-  write(*,*) 'pi is ', sum*step
-  
+  write(*,*) 'pi is ', summ*step
   
 END PROGRAM ex1
